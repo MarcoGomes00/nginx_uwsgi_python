4f6b9b9b606d13a4cd01bd6396b0f7c635b2a409
Marco Gomes <ext.marco.gomes@farfetch.com>
2020-09-24 23:25:20 +0100

    [FEAT] clean up

--- app/main.py
+++ app/main.py
@@ -6,17 +6,3 @@ import sys
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     return [b"Hello World-XPTO"]  # python3
-
-
-def main(args):
-    print('Text :\t{}'.format(args.text))
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--url", dest="text",
-                        default="default text", help="Write what you want")
-    parser.add_argument("-v", "--verbose", dest="verbose", action="count",
-                        default=0, help="Verbosity (-v, -vv, etc)")
-    arguments = parser.parse_args()
-    sys.exit(main(arguments))
